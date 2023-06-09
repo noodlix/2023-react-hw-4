a9b1765b6ea6975d1f0a08325ab51ba42ab73952
zhibek ʚ♡ɞ <111293401+noodlix@users.noreply.github.com>
2023-06-09 12:56:01 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,11 @@
+'use client';
+import Oneuser from '../app/components/oneuser'
 export default function Home() {
-    return <main></main>;
+
+  return <main>
+          <p className="text-2xl text-white">Welcome</p>
+      <div className="mt-16 flex flex-col items-center">
+          <Oneuser/>
+      </div>
+  </main>;
 }
